c33a91fe124aaa14e281a1343d5483c95e473d3b
vicky-patil98 <161872885+vicky-patil98@users.noreply.github.com>
2024-03-07 20:06:31 +0530

    Delete Day4 directory

--- Day4/CatheyBankException.java
+++
@@ -1,7 +0,0 @@
-package com.learning.core.day4;
-//D04P03
-class CatheyBankException extends Exception {
-    public CatheyBankException(String message) {
-        super(message);
-    }
-}
--- Day4/LowBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.learning.core.day4;
-//D04P01-3
-public class LowBalanceException extends Exception {
-    public LowBalanceException() {
-        super("LowBalance");
-    }
-}
